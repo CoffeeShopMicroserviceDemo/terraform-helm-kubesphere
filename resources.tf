707abcd06ebf35935e35ad970458f12c3291aa84
Owen Hayes <owen.v.hayes@gmail.com>
2021-09-14 20:43:05 -0700

    Added index to metadata block in kubesphere_namespace

--- resources.tf
+++ resources.tf
@@ -8,7 +8,7 @@ resource "helm_release" "kubesphere" {
   name = "kubesphere"
   repository = var.kubesphere_helm_repository
   chart = "ks-installer"
-  namespace = kubernetes_namespace.kubesphere_namespace.metadata.name
+  namespace = kubernetes_namespace.kubesphere_namespace.metadata[0].name
 
   set {
     name = "service.type"
