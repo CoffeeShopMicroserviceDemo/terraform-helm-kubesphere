5a674e06eb03286f2b443a540a5a352eddba23bc
Owen Hayes <owen.v.hayes@gmail.com>
2021-09-14 21:05:57 -0700

    Added automatic namespace creation

--- resources.tf
+++ resources.tf
@@ -1,14 +1,9 @@
-resource "kubernetes_namespace" "kubesphere_namespace" {
-  metadata {
-    name = var.kubesphere_namespace
-  }
-}
-
 resource "helm_release" "kubesphere" {
   name = "kubesphere"
   repository = var.kubesphere_helm_repository
   chart = "ks-installer"
-  namespace = kubernetes_namespace.kubesphere_namespace.metadata[0].name
+  namespace = var.kubesphere_namespace
+  create_namespace = true
 
   set {
     name = "service.type"
